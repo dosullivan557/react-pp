641b5518b4fc2d30209cb0bc05fc61c279e633e8
Danny <dosullivan557@gmail.com>
2023-02-09 21:23:12 +0000

    cleanup

--- index.js
+++
@@ -1,6 +0,0 @@
-export { default as useToggle } from './useToggle';
-export { default as useAsyncHook } from './useAsyncHook';
-export { default as useScreenDimensions } from './useScreenDimensions';
-export { default as useValidatedForm } from './useValidatedForm';
-export { default as useDebounce } from './useDebounce';
-export * as regex from './regex';
