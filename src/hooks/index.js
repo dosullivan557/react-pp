4891cf491cdd50f112d315b14fc4b0a2c19a6083
Danny <dosullivan557@gmail.com>
2023-03-15 09:37:17 +0000

    new hooks

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -3,3 +3,5 @@ export { default as useAsyncHook } from './useAsyncHook';
 export { default as useScreenDimensions } from './useScreenDimensions';
 export { default as useValidatedForm } from './useValidatedForm';
 export { default as useDebounce } from './useDebounce';
+export { default as useFetch } from './useFetch';
+export { default as useTimeout } from './useTimeout';
